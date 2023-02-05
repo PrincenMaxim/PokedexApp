d52b59cbc06fcc8f612df7707e0f56af41721bcd
Maxim <maxim.princen@geosolutions.be>
2023-02-05 21:18:26 +0100

    Dynamic screen coloring

--- pokedex-app/src/components/overview-components/PokeInfoView.vue
+++ pokedex-app/src/components/overview-components/PokeInfoView.vue
@@ -1,47 +1,74 @@
 <template>
-    <section>
-        <div v-if="!pokemonStore.isFetching">
-            {{ selectedPokemon }}
-        </div>
-    </section>
+  <section>
+    <div
+      v-if="!pokemonStore.isFetching && selectedPokemon"
+      :style="{ backgroundColor: styleColor }"
+      class="pokeinfo-container">
+      <h1>{{ selectedPokemon["name"] }}</h1>
+    </div>
+  </section>
 </template>
 
 <script>
-import { usePokemonStore } from '@/stores/SelectedPokemonStore';
-
+import { usePokemonStore } from "@/stores/SelectedPokemonStore";
+import { typeColor } from "@/typeColor.js";
 
 export default {
-    data(){
-        return {
-            pokemonStore: usePokemonStore(),
-            selectedPokemon: null,
-            isFetching: true
-        }
-        
-    },
+  data() {
+    return {
+      pokemonStore: usePokemonStore(),
+      selectedPokemon: null,
+      isFetching: true,
+      primaryType: null,
+    };
+  },
 
-    method:{
-        
+  method: {},
+  watch: {
+    $route() {
+      this.selectedPokemon = this.pokemonStore.getPokemonById(
+        parseInt(this.$route.params.pokeid)
+      );
+      this.primaryType = this.selectedPokemon.types[0].type.name;
     },
-    watch: {
-        $route(){
-            this.selectedPokemon = this.pokemonStore.getPokemonById(parseInt(this.$route.params.pokeid));
-        },
-        isFetching(){
-            if(this.isFetching == false){
-                this.selectedPokemon = this.pokemonStore.getPokemonById(parseInt(this.$route.params.pokeid));
-            }
-        }
+    isFetching() {
+      if (this.isFetching == false) {
+        this.selectedPokemon = this.pokemonStore.getPokemonById(
+          parseInt(this.$route.params.pokeid)
+        );
+        this.primaryType = this.selectedPokemon.types[0].type.name;
+      }
     },
-    created(){
-        this.isFetching = this.pokemonStore.isFetching;
-        
-        
-    }
-   
-}
+  },
+  computed: {
+    styleColor() {
+      let color = typeColor(this.primaryType);
+      return color;
+    },
+  },
+  created() {
+    this.isFetching = this.pokemonStore.isFetching;
+  },
+};
 </script>
 
-<style>
+<style scoped>
+section {
+  height: 100%;
+  width: 100%;
+}
+.pokeinfo-container {
+  filter: brightness(110%);
+  filter: opacity(90%);
+  height: 100%;
+  width: 100%;
+}
 
+h1 {
+  color: #ffffff;
+  font-style: normal;
+  font-weight: 700;
+  font-size: 3vw;
+  letter-spacing: 0.01vw;
+}
 </style>
