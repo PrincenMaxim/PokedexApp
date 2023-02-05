d2b8b081a703c119f4c02e90c5fb60e6b17bbcbb
Maxim <maxim.princen@geosolutions.be>
2023-02-05 14:33:04 +0100

    Finished on lIstView

---
+++ pokedex-app/src/components/overview-components/StandardView.vue
@@ -0,0 +1,22 @@
+<template>
+    <section>
+        <poke-logo></poke-logo>
+    </section>
+        
+</template>
+<script>
+import PokeLogo from '../global-components/PokeLogo.vue';
+export default {
+  components: { PokeLogo },
+}
+</script>
+<style scoped>
+section {
+    height: 100%;
+    width: 100%;
+    display: flex;
+    flex-direction: column; 
+}
+
+
+</style>
